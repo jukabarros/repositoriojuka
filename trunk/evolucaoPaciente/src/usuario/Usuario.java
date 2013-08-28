97b866f6f56a86cd1f605a8be2305034a75d4a7d
juccelino.barros@gmail.com <juccelino.barros@gmail.com@94132d7c-afc8-b231-33b5-c2d0a7999199>
2013-08-28 00:10:49 +0000

    Usuario

---
+++ trunk/evolucaoPaciente/src/usuario/Usuario.java
@@ -0,0 +1,115 @@
+package usuario;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="usuario")
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 6581205662725060820L;
+
+	@Id 
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id_usuario", unique=true, nullable=false)
+	private int idUsuario;
+	
+	@Column(name="cpf", unique=true)
+	private String cpf;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="senha")
+	private String senha;
+	
+	@Column(name="login", unique=true)
+	private String login;
+	
+	@Column(name="telefone")
+	private String telefone;
+	
+	public Usuario() {
+	}
+
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + idUsuario;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (idUsuario != other.idUsuario)
+			return false;
+		return true;
+	}
+	
+	
+}
