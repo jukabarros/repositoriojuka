daeb1515088f1f31ecd0bdedf4e4744c58c6e8d6
juccelino.barros@gmail.com <juccelino.barros@gmail.com@94132d7c-afc8-b231-33b5-c2d0a7999199>
2014-05-14 15:37:09 +0000

    Inicio IMP chat

---
+++ domino_rest/src/chat/Chat.java
@@ -0,0 +1,69 @@
+package chat;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+public class Chat implements Serializable{
+
+	private static final long serialVersionUID = 8874494149275732959L;
+	
+	@XmlElement
+	private Long id;
+	
+	@XmlElement
+	private String msg;
+	
+	@XmlElement
+	private String login;
+	
+	@XmlElement
+	private Date date;
+
+	
+	public Chat() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+
+}
