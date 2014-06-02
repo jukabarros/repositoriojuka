d4f0a98de47412208b06d4be5ecfd0bf5ccd3041
juccelino.barros@gmail.com <juccelino.barros@gmail.com@94132d7c-afc8-b231-33b5-c2d0a7999199>
2014-06-02 22:06:38 +0000

    IMP do CHAT VIA TCP ERRO A PARTIR DA SEGUNDA MSG
    Criação do pacote de teste, caso precisar.

--- domino_tcp/src/server/TCPServer.java
+++ domino_tcp/src/server/TCPServer.java
@@ -2,12 +2,16 @@ package server;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+
+import config.ReadProperties;
 import model.MsgChat;
 
 
@@ -18,40 +22,53 @@ public class TCPServer implements Serializable {
 	private List<MsgChat> chatList;
 	static TCPServer server;
 	private static ServerSocket welcomeSocket;
-
-	public TCPServer() {
+	
+	private ReadProperties readProperties = new ReadProperties();
+	private Properties properties;
+	private String socketPort;
+	private static int socketPortint;
+	private static String serverResponse;
+	
+	public TCPServer() throws IOException {
 		chat = new MsgChat();
 		chatList = new ArrayList<MsgChat>();
-		System.out.println("TCP Server iniciado...");
+		properties = readProperties.getProp();
+		socketPort = properties.getProperty("port.tcp.server");
+		socketPortint = Integer.parseInt(socketPort);
+		
 	}
 	
 	public static void main(String argv[]) throws Exception {
 		String clientCommand;
 		server = new TCPServer();
-		welcomeSocket = new ServerSocket(1901);
+		welcomeSocket = new ServerSocket(socketPortint);
+		System.out.println("TCP Server iniciado na porta: "+socketPortint);
 		while(true) {
-				
-	          Socket connectionSocket = welcomeSocket.accept();
-	          BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-	          DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-	          clientCommand = inFromClient.readLine();
+			Socket connectionSocket = welcomeSocket.accept();
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+			clientCommand = inFromClient.readLine();
+			
+			System.out.println("**** SERVIDOR ****");
+			System.out.println("Comando do Cliente: " +clientCommand);
+			/*
+			 * Tratamento dos Comandos recebidos do Cliente
+			 */
+			String[] brokenString = clientCommand.split("::");
+			String tcpCommandClient = brokenString[0]; // String de Comando do cliente
+			if (tcpCommandClient.equals("sendChatMsg")){
+				serverResponse = "Retorno Cliente\n";
+				System.out.println("MSG CHEGOU NO SERVIDOR");
+				outToClient.writeBytes(serverResponse);
+				System.out.println("SERVER RESPONSE: "+serverResponse);
+			}
 
-	          /*
-	           * Tratamento dos Comandos recebidos do Cliente
-	           */
-	          String[] brokenString = clientCommand.split("::");
-	          String tcpCommandClient = brokenString[0]; // String de Comando do cliente
-	          if (tcpCommandClient.equals("sendChatMsg")){
-	        	  System.out.println("MSG CHEGOU NO SERVIDOR");
-	        	  outToClient.writeBytes(clientCommand);
-	          }
-	          
 		} // Fechando o WHILE
-		
+
 	} // Fechando o MAIN
-	
-	
-	
+
+
+
 	
 	/*
 	 * GET AND SET
