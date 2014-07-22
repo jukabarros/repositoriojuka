caecbe6d82ecf3222961e2f0862f740099436da4
juccelino.barros@gmail.com <juccelino.barros@gmail.com@94132d7c-afc8-b231-33b5-c2d0a7999199>
2014-07-22 21:44:54 +0000

    Implementacao da Leitura do arquivo

--- trunk/count_hour/src/controller/TimeWorkController.java
+++ trunk/count_hour/src/controller/TimeWorkController.java
@@ -1,10 +1,15 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Serializable;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
 import model.TimeWork;
@@ -13,7 +18,7 @@ import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 
 @ManagedBean(name="twController")
-@RequestScoped
+@ViewScoped
 public class TimeWorkController implements Serializable {
 
 	private static final long serialVersionUID = -3799636315200412151L;
@@ -28,17 +33,42 @@ public class TimeWorkController implements Serializable {
 		timeWork = new TimeWork();
 	}
 	
-	/*
-	 * Fazer validacao do Arquivo .txt
-	 */
-	public void doUpload(FileUploadEvent fileUploadEvent) {
-		UploadedFile uploadedFile = fileUploadEvent.getFile(); 
+	public void doUpload(FileUploadEvent fileUploadEvent) throws IOException {
+		UploadedFile uploadedFile = fileUploadEvent.getFile();
+
 		String fileNameUploaded = uploadedFile.getFileName();
-		long fileSizeUploaded = uploadedFile.getSize();
-		String infoAboutFile = "<br/> Arquivo recebido: <b>" +fileNameUploaded +"</b><br/>"+ "Tamanho do Arquivo: <b>"+fileSizeUploaded+"</b>"; 
-		FacesContext facesContext = FacesContext.getCurrentInstance(); 
-		facesContext.addMessage(null, new FacesMessage("Sucesso", infoAboutFile));
+		String fileType = uploadedFile.getContentType();
+		if (fileType.equals("text/plain")){
+			String infoAboutFile = "<br/> Arquivo recebido: <b>" +fileNameUploaded +"</b><br/>"+ "Tipo do Arquivo: <b>"+fileType+"</b>"; 
+//			FacesContext facesContext = FacesContext.getCurrentInstance(); 
+//			facesContext.addMessage(null, new FacesMessage("", infoAboutFile));
+			readFile(uploadedFile);
+		}else{
+			System.out.println("ELSE");
+			FacesContext facesContext = FacesContext.getCurrentInstance();
+			facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro: O arquivo precisa ser .txt", "")); //Mensagem de Erro
+		}
+	}
 	
+	/*
+	 * Ler e alimentar o data table
+	 */
+	public void readFile(UploadedFile file) throws IOException {
+		System.out.println("** Lendo Arquivo TXT");
+		InputStream is = file.getInputstream();
+		BufferedReader bfReader = null;
+		try{
+			is = new ByteArrayInputStream(file.getContents());
+			bfReader = new BufferedReader(new InputStreamReader(is));
+			String temp = null;
+			while((temp = bfReader.readLine()) != null){
+				System.out.println(temp);
+			}
+			
+		}catch(Exception ex){
+			System.err.println("Erro readFile Exception: "+ex.getMessage());
+		}
+		
 	}
 
 	/*
