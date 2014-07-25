7fd197b22605628eb11f78824941b54cfce10ebb
juccelino.barros@gmail.com <juccelino.barros@gmail.com@94132d7c-afc8-b231-33b5-c2d0a7999199>
2014-07-25 19:39:42 +0000

    Finalizando o calculo das horas e a alimentacao do arrayList que sera exibido no datatable.
    Falta Fazer:
    Datatable Editavel(Campo meio expediente) na view.
    Registros de Faltas
    
    Erros:
    
    Quando o formulario da erro, caso o usuario insira de novo a planilha
    eh lida de novo.
    Opcoes:
    1) Leitura feita, desabilita o upload
    2) Refaz o upload

--- trunk/count_hour/src/controller/TimeWorkController.java
+++ trunk/count_hour/src/controller/TimeWorkController.java
@@ -24,9 +24,6 @@ import jxl.Workbook;
 import jxl.read.biff.BiffException;
 import model.TimeWork;
 
-import org.joda.time.Days;
-import org.joda.time.Hours;
-import org.joda.time.Minutes;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 
@@ -38,8 +35,11 @@ public class TimeWorkController implements Serializable {
 	private static final long serialVersionUID = -3799636315200412151L;
 	
 	private TimeWork timeWork;
+	private ArrayList<TimeWork> timeWorkList;
 	private ArrayList<String> invalidDateStr;
 	private ArrayList<String> allValidDateStr;
+	private String nameForm;
+	private String hoursDayForm;
 	
 	public TimeWorkController() {
 		super();
@@ -47,6 +47,9 @@ public class TimeWorkController implements Serializable {
 		timeWork = new TimeWork();
 		invalidDateStr = new ArrayList<String>();
 		allValidDateStr = new ArrayList<String>();
+		timeWorkList = new ArrayList<TimeWork>();
+		nameForm = null;
+		hoursDayForm = null;
 		
 	}
 	
@@ -55,27 +58,22 @@ public class TimeWorkController implements Serializable {
 
 		String fileNameUploaded = uploadedFile.getFileName();
 		String fileType = uploadedFile.getContentType();
-		
-		
-		String infoAboutFile = null;
 		FacesContext facesContext = FacesContext.getCurrentInstance();
+
 		// Verificando o tipo de arquivo
 		if (fileType.equals("text/plain")){
 			
-			readTXTFile(uploadedFile);
+			facesContext = FacesContext.getCurrentInstance(); 
+			facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Arquivo TXT Recebido: "+fileNameUploaded, ""));
 			
-			infoAboutFile = "<br/> Arquivo TXT recebido: <b>" +fileNameUploaded +"</b>"; 
-			facesContext.addMessage(null, new FacesMessage("", infoAboutFile));
+			readTXTFile(uploadedFile);
 		}else if(fileType.equals("application/vnd.ms-excel")){
 			
-			
-			infoAboutFile = "<br/> Arquivo XLS recebido: <b>" +fileNameUploaded +"</b>"; 
 		    facesContext = FacesContext.getCurrentInstance(); 
-			facesContext.addMessage(null, new FacesMessage("", infoAboutFile));
+		    facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Arquivo XLS Recebido: "+fileNameUploaded, ""));
 
 			readXLSFile(uploadedFile);
-		}
-		else{
+		}else{
 			facesContext = FacesContext.getCurrentInstance();
 			facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro: O arquivo precisa ser .txt ou .xls", "")); //Mensagem de Erro
 		}
@@ -102,24 +100,24 @@ public class TimeWorkController implements Serializable {
 		
 	}
 	
-	/*
-	 * Ler o arquivo XLS
+	/**
+	 * Ler o arquvi XLS
+	 * @param file
+	 * @throws BiffException
+	 * @throws IOException
 	 */
 	public void readXLSFile(UploadedFile file) throws BiffException, IOException{
 		Workbook wb = Workbook.getWorkbook(file.getInputstream());
 		Sheet sheet = wb.getSheet(0);
 		int line = sheet.getRows(); //Capturando as linhas da planilha
-		
+
 		ArrayList<Date> allHoursByDate = new ArrayList<Date>();
 		int nextCell = 0;
 		for(int i = 0; i < line; i++  ){
 
-			Cell cell1 = sheet.getCell(0, i); // Siape
-
+//			Cell cell1 = sheet.getCell(0, i); // Siape
+//			Cell cell3 = sheet.getCell(2, i); // Dia da semana
 			Cell cell2 = sheet.getCell(1, i); // Data
-
-			Cell cell3 = sheet.getCell(2, i); // Dia da semana
-
 			Cell cell4 = sheet.getCell(3, i); // horas
 
 			nextCell = i+1;
@@ -147,21 +145,24 @@ public class TimeWorkController implements Serializable {
 
 					allHoursByDate = new ArrayList<Date>(); // Iniciando um novo calculo
 				}
-				
 
 			}
-
 		}
 		// Listando as datas que nao foram validadas
-		checkInvalidDate();
+		listInvalidDate();
 		invalidDateStr = new ArrayList<String>();
-		
+		allValidDateStr = new ArrayList<String>();
 		// Fechando a planilha
 		wb.close();
 	}
 	
-	/*
+	/**
 	 * Validar o campo Data e Hora da planilha
+	 * DateLine -> Server identificar onde ocorreu o erro na planilha
+	 * 
+	 * @param dateStr
+	 * @param dateLine
+	 * @return
 	 */
 	public Date stringToHourDate(String dateStr, int dateLine){
 		FacesContext facesContext = FacesContext.getCurrentInstance();
@@ -178,18 +179,22 @@ public class TimeWorkController implements Serializable {
 	}
 	
 	
-	/*
+	/**
 	 * Diferenciar Entrada e saida
 	 * Considerando que a primeira hora do dia seja a hora de entrada
 	 * e a segunda de saida e assim por diante.
 	 * Caso o numero de horas do dias seja impar, ficara impossivel de fazer
 	 * o calulo, o metodo retornara nulo.
+	 * 
+	 * Mandar para camada de Servico????
+	 * 
+	 * @param listHours
+	 * @param dateStr
 	 */
 	public void hoursCalculatorByDay(ArrayList<Date> listHours, String dateStr){
 		try{
 			Collections.sort(listHours); // Ordenando a lista
-			
-			// Fazer o calculo das horas, add no data table
+			DateFormat formatter = new SimpleDateFormat("HH:mm"); // Usado para concatenar as var long de diff horas e minutos
 			
 			if (listHours.size()%2 != 0){
 				this.invalidDateStr.add(dateStr);
@@ -210,22 +215,30 @@ public class TimeWorkController implements Serializable {
 					
 						Date in = listHours.get(j);
 						Date out = listHours.get(j+1);
-						j = j+2; // Pegar a proxima entrada
+						j = j+2; // Pegar a proxima entrada e saida
 						long diff = out.getTime() - in.getTime();
+						
 						long diffHours = diff / (60 * 60 * 1000) % 24;
 						long diffMinute = diff / (60 * 1000) % 60;
 
 						diffTotal += diff; // Incrementando as horas trabalhadas no dia
 						System.out.println("\nDiferenca em Horas: "+diffHours+ " em minutos: "+diffMinute);
 						
-						// Fazer o calcula da diferenca das horas
 					}
 				}
 				diffTotalHours = diffTotal/ (60 * 60 * 1000) % 24;
 				diffTotalMinutes = diffTotal/ (60 * 1000) % 60;
-				// Converter pra Date ou Gregoriam ou deixar em Long????
 				System.out.println("Total: "+diffTotalHours+":"+diffTotalMinutes);
 				
+				String hoursWorkedStr = diffTotalHours+":"+diffTotalMinutes;
+				Date hoursWorked = (Date)formatter.parse(hoursWorkedStr);
+				
+				this.timeWork.setWorkDayDateStr(dateStr);
+				this.timeWork.setHoursWorked(hoursWorked);
+				this.timeWork.setHalfTime(false); // So sera setado pelo usuario no proprio datatable
+				this.timeWorkList.add(this.timeWork);
+				
+				this.timeWork = new TimeWork();
 				
 			}
 			
@@ -235,11 +248,41 @@ public class TimeWorkController implements Serializable {
 		}
 	}
 	
-	/*
+	/**
+	 * Alimentando a lista para criar o datatable
+	 * retorna vazio para tirar o erro na view
+	 * @return
+	 */
+	public String createDataTable(){
+		FacesContext fContext = FacesContext.getCurrentInstance();
+		try{
+			int hoursDayInt = Integer.parseInt(getHoursDayForm());
+			if (hoursDayInt <= 24 && hoursDayInt > 0){
+				for (int i = 0; i < this.timeWorkList.size(); i++) {
+					this.timeWorkList.get(i).setName(getNameForm());
+					this.timeWorkList.get(i).setHoursDay(hoursDayInt);
+					
+				}
+				
+			}else{
+				System.err.println("\n***Erro: Campo horas diarias tem que ser entre 1 e 24");
+				fContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro: O campo Horas Diárias tem que ser entre 1 e 24", ""));
+				
+			}
+			System.out.println("\n\t****Tamanho da Nova Lista: "+this.timeWorkList.size());
+			
+		}catch(Exception pe){
+			fContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro no campo Horas Diárias", ""));
+			System.err.println("*** Erro: nao foi possivel converter o valor para inteiro "+getHoursDayForm());
+		}
+		return null;
+	}
+	
+	/**
 	 * Metodo responsavel por checar e listar as datas invalidas,
 	 * Pode ser por formato invalido, numero de horas impar
 	 */
-	public void checkInvalidDate(){
+	public void listInvalidDate(){
 		FacesContext fc = FacesContext.getCurrentInstance();
 		if(!this.invalidDateStr.isEmpty()){
 			fc.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Data(s) Inválida(s) devido ao Número ímpar de horas: ", "")); //Mensagem de Erro
@@ -263,6 +306,38 @@ public class TimeWorkController implements Serializable {
 
 	public void setTimeWork(TimeWork timeWork) {
 		this.timeWork = timeWork;
+	}
+
+	public ArrayList<TimeWork> getTimeWorkList() {
+		return timeWorkList;
+	}
+
+	public void setTimeWorkList(ArrayList<TimeWork> timeWorkList) {
+		this.timeWorkList = timeWorkList;
+	}
+
+	public ArrayList<String> getInvalidDateStr() {
+		return invalidDateStr;
+	}
+
+	public void setInvalidDateStr(ArrayList<String> invalidDateStr) {
+		this.invalidDateStr = invalidDateStr;
+	}
+
+	public String getNameForm() {
+		return nameForm;
+	}
+
+	public void setNameForm(String nameForm) {
+		this.nameForm = nameForm;
+	}
+
+	public String getHoursDayForm() {
+		return hoursDayForm;
+	}
+
+	public void setHoursDayForm(String hoursDayForm) {
+		this.hoursDayForm = hoursDayForm;
 	}
 	
 	
